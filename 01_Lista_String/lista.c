6e9cb70fb9070357d5daefd081985bc503425c31
Vitor Yuji <83984086+VitorYuji25@users.noreply.github.com>
2022-06-23 20:22:01 -0300

    Update lista.c
    Signed-off-by: Vitor Yuji <83984086+VitorYuji25@users.noreply.github.com>

--- 01_Lista_String/lista.c
+++ 01_Lista_String/lista.c
@@ -1,146 +1,333 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "Lista.h"
-#define MAXS 20
-#cade o Rafa
-# estou aqui !!!!! 
-struct strLista{
-	char arrStr[25][16];
-	int fim; // indica qnts string tenho na Listaa
+#include "lista.h"
 
-};
+// Prototipo da função imprime_lista.
+void imprime_lista(Lista);
 
-Lista* cria_lista(){
-	Lista* lst = (Lista*)malloc(sizeof(Lista));
-	if(lst != NULL)
-		lst->fim = 0;
+int main(){
+int op; // Variavel responsavel por receber a opção desejada do menu
+int flag = 0,flag2 = 0; //Flag contabiliza a quantidade de especificações da lista (0 - lista nao criada | 1 - Lista criada)
+int resp; // Variavel responsavel por selecionar se a operação será realizada na lista 1 ou lista 2
+Lista *l1; Lista *l2; Lista *l3; // Declaração das "listas".
 
-	return lst;
-}
+    do{ // Programa
+        do{ //Menu
+            //Obtem a opção do usuario
+            printf("\n------------------- MENU -------------------\n");
+            printf("\n [1] Criar uma lista");
+            printf("\n [2] Esvaziar a lista");
+            printf("\n [3] Apagar a lista");
+            printf("\n [4] Inserir um elemento na lista");
+            printf("\n [5] Remover um elemento na lista");
+            printf("\n [6] Imprimir a lista");
+            printf("\n [7] Remove negativos");
+            printf("\n [8] Remove par");
+            printf("\n [9] Intercala listas");
+            printf("\n [10] Verificar o tamanho da lista:");
+            printf("\n [11] Sair do sistema");
+            printf("\n\n Digite a opcao desejada: ");
+            printf("\n\n------------------------------------------\n");
 
-int lista_vazia(Lista* lst){
-	if(lst == NULL || lst->fim == 0)
-		return 1;
-	else
-		return 0;
-}
+            scanf("%d", &op);
+            setbuf(stdin,NULL);
 
-int lista_cheia(Lista* lst){
-	if(lst->fim == MAXS)
-		return 1;
-	else
-		return 0;
-}
+            if(op < 1 || op > 11)
+            printf("\n *** Opcao digitada invalida! As opcoes disponiveis são:");
 
-int inserir_elem(Lista* lst, char str[]){
-	if(lst == NULL || lista_cheia(lst))
-		return 0;
+        } while(op < 1 || op > 11);
 
-	strcpy(lst->arrStr[ lst->fim ], str);
-	lst->fim ++;
+        if (op == 11) break; //Sair do sistema
 
-	return 1;
-}
+        else if(op != 1 && flag == 0) {
+            printf("\n\n Antes de qualquer operacao, a lista 1 precisa ser CRIADA.");
+            continue;
+            }
 
-int insere_ord(Lista* lst, char string[]){
-	if(lst == NULL || lista_cheia(lst))
-		return 0;
 
-	// se Listaa vazia ou se for na ultima posicao
-	if( (lista_vazia(lst)) || (strcmp(string, lst->arrStr[lst->fim - 1]) <= 0) ){
-		strcpy(lst->arrStr[ lst->fim ], string);
 
-	}else{
-		int idx = 0;
-		while( ((idx < lst->fim) && (strcmp(string, lst->arrStr[idx]) < 0))
-	      ||   ((idx < lst->fim) && (strcmp(string, lst->arrStr[idx]) == 0) ))
-			idx ++;
 
-		for(int i = lst->fim; i > idx; i--){
-			strcpy(lst->arrStr[i], lst->arrStr[i-1]);
-		}
+        switch(op){
+        case 1: { // Criar uma lista:
+                printf("\n Digite [1] para criar na lista 1 e [2] para criar na lista 2: \n");
+                scanf("%d", &resp);
 
-		// posicao idx esta livre
-		strcpy(lst->arrStr[idx], string);
+                if (resp == 1){
+                    l1 = cria_lista();
+                    if(l1 == NULL){
+                        printf("\n Falha na criacao da lista");
+                        return -1;
+                }
+                printf("\n A lista foi CRIADA com sucesso!");
+                flag++; //Quantidade de vezes que a lista 1 foi criada
+                break;
+                }
+                else if(resp == 2){
+                    l2 = cria_lista();
+                    if(l2 == NULL){
+                        printf("\n Falha na criacao da lista");
+                        return -1;
+                }
+                printf("\n A lista foi CRIADA com sucesso!");
+                flag2++;
+                break;
+                }
+                else{
+                    printf("\n Opção inválida");
+                    break;
+                }
+        }
 
-	}
+        case 2:{ // Esvaziar a lista:
+            printf("\n Digite [1] para esvaziar a lista 1 e [2] para esvaziar a lista 2: \n");
+            scanf("%d", &resp);
+            if (resp == 1){
+                if(esvazia_lista(*l1) == 0){
+                    printf("\n Nao foi possivel esvaziar a lista.");
+                    return -1;
+            }
+            printf("\n A lista foi ESVAZIADA com sucesso!");
+            break;
+            }
 
-	lst->fim ++;
-	return 1;
-}
+            else if(resp == 2){
+                if(esvazia_lista(l2) == 0){
+                printf("\n Nao foi possivel esvaziar a lista.");
+                return -1;
+            }
+            printf("\n A lista foi ESVAZIADA com sucesso!");
+            break;
+            }
 
-int remove_elem(Lista* lst, char str[]){
-	if(lst == NULL || lista_vazia(lst))
-		return 0;
+            else{
+                printf("\n Opção inválida");
+                break;
+            }
+        }
+        /*
+        case 3:{ // Apagar
+            printf("\n Digite [1] para apagar na lista 1 e [2] para apagar na lista 2: \n");
+            scanf("%d", &resp);
+            if (resp == 1){
+                if(apaga_lista(&l) == 0){
+                    printf("\n Nao foi possivel apagar a lista 1.");
+                    return -1;
+                    }
+                    printf("\n lista 1 foi APAGADA com sucesso!");
+                    flag = 0;
+                    break;
+                }
+                else if(resp == 2){
+                    if(apaga_lista(&l2) == 0){
+                    printf("\n Nao foi possivel apagar a lista 2.");
+                    return -1;
+                    }
+                    printf("\n lista 2 foi APAGADA com sucesso!");
+                    flag2 = 0;
+                    break;
+                    }
+                    else{
+                        printf("\n Opção inválida");
+                        break;
+                        }
+                    }*/
 
-	int idx = 0;
-	while( idx < lst->fim && strcmp(str, lst->arrStr[ idx ]) )
-		idx ++;
+        case 4:{ //Inserir elemento na lista
+            int n; //Elemento digitado
+            printf("\n Digite o elemento (int) a ser inserido na lista: ");
+            scanf("%d", &n);
 
-	if(idx == lst->fim)
-		return 0;
-	// idx == pos da str que quero remover
-	for(int i = idx; i < (lst->fim)-1; i++)
-		strcpy(lst->arrStr[i], lst->arrStr[i+1]);
+            printf("\n Digite [1] para inserir na lista 1 e [2] para inserir na lista 2: \n");
+            scanf("%d", &resp); // resposta da operação
 
-	lst->fim --;
-	return 1;
-}
+            // Tratamentos:
+            if (resp == 1){
+                if (inserir_elem(1l,n) == 0)
+                printf("\n A lista esta cheia! Nao foi possivel incluir o elemento %d. ",n);
+            else
+                printf("\n O elemento %d foi incluido com sucesso! ",n);
+            break;
+                }
+                else if(resp == 2){
+                if (inserir_elem(l2,n) == 0){
+                    printf("\n A lista esta cheia! Nao foi possivel incluir o elemento %d. ",n);
+                    }else{
+                        printf("\n O elemento %d foi incluido com sucesso! ",n);
+                }
+                        break;
+                }
+                else{
+                    printf("\n Opção inválida");
+                    break;
+                    }
+        }
 
-char* getPos(Lista* lst, int idx){
-	if(idx < lst->fim)
-		return lst->arrStr[idx];
+        case 5:{    // Remove um elemento na lista
+            int n;
+             printf("\n Digite o elemento (int) a ser excluido na lista: ");
+             scanf("%d", &n);
 
-	return NULL;
-}
+             printf("\n Digite [1] para remover na lista 1 e [2] para remover na lista 2: \n");
+             scanf("%d", &resp);
 
-int tamanho(Lista* lst){
+                if (resp == 1){
+                if(remove_todas(l1,n) == 0){
+                    if(lista_vazia(l1) == 1)
+                        printf("\n A lista 1 ja esta vazia!");
+                    else
+                        printf("\n Nao existe o elemento %d na lista! ",n);
+                        }
+                else
+                    printf("\n o elemento %d foi removido com sucesso! ",n);
+                    break;
+                }
+                else if(resp == 2){
+                    if(remove_todas(l2,n) == 0){
+                        if(lista_vazia(l2) == 1)
+                            printf("\n A lista 2 ja esta vazia!");
+                    else
+                        printf("\n Nao existe o elemento %d na lista! ",n);
+            }
+            else
+                printf("\n o elemento %d foi removido com sucesso! ",n);
+                break;
+            }
+            else{
+                printf("\n Opção inválida");
+                break;
+                }
+        }
+        /*
+        case 6:{ // Imprime a lista
+             printf("\n Digite [1] para imprimir a lista 1 e [2] para imprimir a lista 2 e [3] para imprimir ambas as listas: \n");
+             scanf("%d", &resp);
 
-	return lst->fim;
-}
+                if (resp == 1){
+                imprime_lista(l);// Interface não é feita no TAD.
+                break;
+                }
+                else if(resp == 2){
+                imprime_lista(l2);
+                break;
+            }
 
-int esvazia_lista(Lista* *lst){
-	if(lista_vazia(*lst)) return 0;
+            else if (resp == 3){
+                imprime_lista(l);
+                imprime_lista(l2);
+                break;
+            }else{
+               printf("\n Opcao invalida! Tente novamente");
+                break;
+            }
 
-	free(*lst);
-	*lst = NULL;
 
-	return 1;
-}
+        }*/
+        /*
+        case 7:{ // Remove posição
+            int pos;
+            printf("\n Digite a posição a ser removida: \n");
+            scanf("%d", &pos);
+            printf("\n Digite [1] para remover negativos da lista 1 e [2] para remover negativos da lista 2: \n");
+            scanf("%d", &resp);
 
-int remove_todas(Lista* lst, char elem[]){
 
-    if(lst == NULL || lista_vazia(lst))
-		return 0; // ERRO, lista vazia ou n iniciliazada
 
-    for(int i= 0; i < lst->fim; i++){
+            if (resp == 1){
+                remove_pos(l,pos);
+                break;
+            }else if(resp == 2){
+                remove_pos(l2,pos);
+                break;
+            }else{
+                printf("\n Opção inválida");
+                break;
+        }
+        }*/
+        case 8:{ // Tamanho
+            printf("\n Digite [1] para remover pares da lista 1 e [2] para remover pares da lista 2: \n");
+            scanf("%d", &resp);
+            int tam;
 
-        if ( strcmp(lst->arrStr[i],elem) == 0 ) {
+                if (resp == 1){
+                    tam = tamanho_lista(l1);
+                    printf("Tamanho da lista 1 eh: %d\n");
+                    break;
+                }else if(resp == 2){
+                    tam = tamanho_lista(l2);
+                    printf("Tamanho da lista 2 eh: %d\n");
+                    break;
+                }else{
+                 printf("\n Opção inválida");
+                 break;
+                 }
+        }
+        /*
+        case 9:{ // Intercala as listas
+           l3 =  intercala_listas(l1,l2);
+            imprime_lista(l3);
+            break;
+        }
 
-            for (int j = i; j < lst->fim-1; j++)
-                strcpy(lst->arrStr[j],lst->arrStr[j+1]); // Deslocamento dos nós sucessores.
+        case 10:{
+        int tamanho; // Variavel que receberá o tamanho da lista.
+        printf("\n Digite [1] para verificar o tamanho da lista 1 e [2] para verificar o tamanho da lista 2: \n");
+        scanf("%d", &resp);
 
-            lst->fim--;
-            i--;
+                if (resp == 1){
+                tamanho_lista(l1);
+                break;
+                }
+                else if(resp == 2){
+                tamanho_lista(l2);
+                break;
+            }else{
+               printf("\n Opcao invalida! Tente novamente");
+                break;
+            }
+
+        }*/
 
         }
-        else
-            continue;
+        }while(op != 11);
+
+        printf("\n\n\t\t Game Over! \n");
+
+        free(l1); //Liberar o espaço alocado para as listas.
+        free(l2);
+        free(l3);
+
+        return 0;
+}
+
+
+/*
+Operação: imprime_lista
+
+- Entrada: Endereço de uma lista
+-  Pré-Condição: Lista existir (Endereço ser valido)
+- Processo: Ao percorrer a lista, apresentamos sua informação na aplicação.
+- Saída: Por se tratar de um retorno VOID, a saida não existe.
+- Pós-condição: Sem pós-condição.
+
+*/
+/*
+void imprime_lista(Lista l){
+    if(lista_vazia(l) == 1){
+        printf("\n Lista VAZIA! ");
+        return;
     }
 
-    return 1;
+    printf("\n\n\n \t LISTA: ");
+    printf("\n\t {");
+    int i;
+    for(i=1;;i++){ //Não sei quantos elementos tem na lista
+        int n; // N representa o elemento presente na lista
+        if(getPos(l,i,&n) == 0){ //Não existe a posição
+            break;
+        }
+        printf(" %d ",n);
 
-        /*
-	int idx = 0;
-	while( idx < lst->fim && strcmp(str, lst->arrStr[ idx ]) )
-		idx ++;
-
-	if(idx == lst->fim)
-		return 0;
-	// idx == pos da str que quero remover
-	for(int i = idx; i < (lst->fim)-1; i++)
-		strcpy(lst->arrStr[i], lst->arrStr[i+1]);
-
-	lst->fim --;*/
+    }
+    printf("} \n Existem %d elementos na lista. \n",i-1); // i-1 será pq o break está dentro do for.
 }
+*/
